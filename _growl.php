2f9b4015b78d1fa99d3c76c4394393a974f425ef
Derek Gifford <derekisbusy@gmail.com>
2017-02-25 12:01:50 -0500

    Add _growl view

---
+++ _growl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Derek Gifford, 2016 - 2017
+ * @package derekisbusy/yii2-flash-growl
+ * @version 1.0.0
+ *
+ * @author derekisbusy https://github.com/derekisbusy
+ */
+
+use derekisbusy\growl\FlashGrowlWidget;
+
+echo  FlashGrowlWidget::widget();
